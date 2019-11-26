c97b810b33673c0b57ca15e158788805be2e2748
Andrew Brey <andrew@fluencyy.com>
2019-11-25 20:54:01 -0800

    feat(memory): seed memory by environment
    use the environment configuration to seed the memory object appropriately

--- src/scripts/global/environment.dev.ts
+++ src/scripts/global/environment.dev.ts
@@ -1,1 +1,61 @@
+import { GlobalMemory } from '../types';
+
 export const DEBUG_ENABLED = true;
+
+export const MEMORY_SEED: GlobalMemory = {
+	enabled: true,
+	baseline: 1,
+	video: {
+		'155ZG0FMHxQ': {
+			id: '155ZG0FMHxQ',
+			speed: 1.3,
+			title: 'Jesse McCartney - Wasted (Official Video)',
+			image:
+				'https://i.ytimg.com/vi/155ZG0FMHxQ/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLD7JZmHKOTCOCAvHrVZF7GM-GuDIA',
+			start: 5,
+			end: 183,
+		},
+		'4SU8gxrhs1g': {
+			id: '4SU8gxrhs1g',
+			speed: 1.7,
+			title: 'Ben Rector - Old Friends (Official Video)',
+			image:
+				'https://i.ytimg.com/vi/4SU8gxrhs1g/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLAfdZ8Tq9cwR1JxxjouSA97xnlzFA',
+			start: 19,
+			end: 235,
+		},
+	},
+	playlist: {
+		'PLNfk_n98B81_LR_mahhT3sOdjCgc-1cuw': {
+			id: 'PLNfk_n98B81_LR_mahhT3sOdjCgc-1cuw',
+			speed: 1.1,
+			title: 'Looper',
+		},
+	},
+	channel: {
+		UCvud0mK7irJ34RVdTPSwDfQ: {
+			id: 'UCvud0mK7irJ34RVdTPSwDfQ',
+			speed: 2,
+			title: 'Quinn XCII',
+			image: 'https://yt3.ggpht.com/a-/AAuE7mDGD7myTMr3OOZbY3fpi6bonAKfnIgCg7bupPoDZQ=s48-c-k-c0xffffffff-no-nd-rj',
+		},
+		UC0A2GKK4UdxtQTxpXYduHUg: {
+			id: 'UC0A2GKK4UdxtQTxpXYduHUg',
+			speed: 2,
+			title: 'Quinn XCII',
+			image: 'https://yt3.ggpht.com/a-/AAuE7mDGD7myTMr3OOZbY3fpi6bonAKfnIgCg7bupPoDZQ=s48-c-k-c0xffffffff-no-nd-rj',
+		},
+		'UCq3Ci-h945sbEYXpVlw7rJg': {
+			id: 'UCq3Ci-h945sbEYXpVlw7rJg',
+			speed: 1.25,
+			title: 'The Chainsmokers',
+			image: 'https://yt3.ggpht.com/a-/AAuE7mBObOXHE8X5QtM1pXX-4GR0GaMMF6tJb6i4C8QYeg=s48-c-k-c0xffffffff-no-nd-rj',
+		},
+		'UCQgUHOPJJrmzCjExg-ISupA': {
+			id: 'UCQgUHOPJJrmzCjExg-ISupA',
+			speed: 1.25,
+			title: 'The Chainsmokers',
+			image: 'https://yt3.ggpht.com/a-/AAuE7mBObOXHE8X5QtM1pXX-4GR0GaMMF6tJb6i4C8QYeg=s48-c-k-c0xffffffff-no-nd-rj',
+		},
+	},
+};
--- src/scripts/global/environment.ts
+++ src/scripts/global/environment.ts
@@ -1,1 +1,5 @@
+import { DEFAULT_GLOBAL_MEMORY } from '../lib/memory/memory.constants';
+import { GlobalMemory } from '../types';
+
 export const DEBUG_ENABLED = false;
+export const MEMORY_SEED: GlobalMemory = DEFAULT_GLOBAL_MEMORY;
